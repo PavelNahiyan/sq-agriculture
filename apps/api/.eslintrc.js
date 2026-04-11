3cda3c64e4a3e4e9b659f20c7a4cac2c9f8a463a
Faisal Nahiyan <faisal.nahiyan@gmail.com>
2026-04-11 17:23:24 +0600

    Add ESLint configuration with TypeScript support

---
+++ apps/api/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', 'node_modules', '*.spec.ts', '*.test.ts'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+};
